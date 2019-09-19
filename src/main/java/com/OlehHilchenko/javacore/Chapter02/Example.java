0cb3c7329d3d312f3dafdc340b3c5c7397723c7f
Oleh Hilchenko <olehhilchenko@gmail.com>
2019-09-19 23:06:49 +0300

    first commit of the project 19.09.2019

---
+++ src/main/java/com/OlehHilchenko/javacore/Chapter02/Example.java
@@ -0,0 +1,10 @@
+package main.java.com.OlehHilchenko.javacore.Chapter02;
+
+/*This is a simple Java program.
+* Name the source file "Example.java"*/
+public class Example {
+    //This program begins with a call method main().
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program");
+    }
+}
