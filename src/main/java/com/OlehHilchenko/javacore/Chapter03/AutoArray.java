c908912ee8ab964965b548928813ef48e34eb5b5
Oleh Hilchenko <olehhilchenko@gmail.com>
2019-09-23 20:17:28 +0300

    fourth commit of the project (Chapter4) 23.09.2019

---
+++ src/main/java/com/OlehHilchenko/javacore/Chapter03/AutoArray.java
@@ -0,0 +1,12 @@
+package main.java.com.OlehHilchenko.javacore.Chapter03;
+
+//improved version of the previous program
+
+public class AutoArray {
+    public static void main(String[] args) {
+
+        int month_days [] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        System.out.println("In april " + month_days[3] + " days.");
+    }
+}
